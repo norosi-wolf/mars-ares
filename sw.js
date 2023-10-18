53804ae474bff24aa2f5afb2c353c366bf320f39
norosi-wolf <110663266+norosi-wolf@users.noreply.github.com>
2023-10-18 12:04:25 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+
+// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+
+// キャッシュ名とキャッシュファイルの指定
+var CACHE_NAME = 'pwa-aest-caches ver1.0.0';
+var urlsToCache = [
+    './',
+    './index.html',
+    './sw.js',
+    './js/',
+    './js/app.js',
+    './js/jquery-3.7.1.min.js',
+    './css/',
+    './css/all.min.css',
+    './css/sanitize.css',
+    './css/style.css',
+    './img/',
+    './img/card.png',
+    './img/heat.png',
+    './img/mc.png',
+    './img/plant.png',
+    './img/steel.png',
+    './img/titanium.png',
+    './img/tr.png',
+    './webfonts/',
+    './webfonts/fa-solid-900.ttf',
+    './webfonts/fa-solid-900.woff2',
+    './webfonts/MPLUSRounded1c-Regular.ttf',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
