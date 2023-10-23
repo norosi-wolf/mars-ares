3f067ff7dcc0a8886da98e710ba147c1841b64ba
norosi-wolf <110663266+norosi-wolf@users.noreply.github.com>
2023-10-23 17:05:51 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const APP_VERSION = '1.0.15';
+const APP_VERSION = '1.0.16';
 const TEMPERATURE_LIST = [-30, -28, -26, -24, -22, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8];
 const OXYGEN_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 const PARTITION_COLOR_LIST = ['#8A2BE2', '#DC143C', '#ffff00', '#FFFFFF']
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-var SW_VERSION = '1.0.15';
+var SW_VERSION = '1.0.16';
 
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 // キャッシュ名とキャッシュファイルの指定
